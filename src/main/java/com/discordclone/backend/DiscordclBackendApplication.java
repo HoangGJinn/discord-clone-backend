3cd6fd1b551ea9f8730ef73695807c96d9f44952
Nguyễn Hoàng Giáp <nguyenhoanggiap2005@gmail.com>
2026-01-22 13:43:08 +0700

    initialize project

---
+++ src/main/java/com/discordclone/backend/DiscordclBackendApplication.java
@@ -0,0 +1,13 @@
+package com.discordclone.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordclBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordclBackendApplication.class, args);
+	}
+
+}
